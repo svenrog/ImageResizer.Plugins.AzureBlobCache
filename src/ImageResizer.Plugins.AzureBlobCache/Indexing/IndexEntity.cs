7247592980cf81e4953bc1a2f20999e6168a442e
Sven-Erik Jonsson <sven.erik@geta.no>
2021-07-12 23:46:03 +0200

    Updated index name of IndexEntity, potential conflicts.

--- src/ImageResizer.Plugins.AzureBlobCache/Indexing/IndexEntity.cs
+++ src/ImageResizer.Plugins.AzureBlobCache/Indexing/IndexEntity.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ImageResizer.Plugins.AzureBlobCache.Indexing
 {
-
+    [Table("ImageResizerCacheIndex")]
     public class IndexEntity
     {
         public IndexEntity() { }
