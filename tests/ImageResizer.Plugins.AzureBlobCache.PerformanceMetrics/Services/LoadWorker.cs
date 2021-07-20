0715210baf7c112990c99661b5ea82a9b7fe83b9
Sven-Erik Jonsson <sven.erik@geta.no>
2021-07-20 19:00:16 +0200

    Removed unused using directive in LoadWorker.

--- tests/ImageResizer.Plugins.AzureBlobCache.PerformanceMetrics/Services/LoadWorker.cs
+++ tests/ImageResizer.Plugins.AzureBlobCache.PerformanceMetrics/Services/LoadWorker.cs
@@ -4,7 +4,6 @@ using ImageResizer.Plugins.AzureBlobCache.PerformanceMetrics.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Timers;
 
